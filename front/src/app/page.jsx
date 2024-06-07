285ddf256fe6cb6d724d9aefdd2fd2ad16b138fc
crespiramiro <crespiramiro@outlook.com>
2024-06-07 00:11:27 -0300

    basic routes setted

---
+++ front/src/app/page.jsx
@@ -0,0 +1,11 @@
+import Routes from "@/Routes"
+
+export default function Home() {
+  return (
+        <div>
+          <h1>Mi Aplicación</h1>
+          <Routes />
+        </div>
+      );
+    };
+  
